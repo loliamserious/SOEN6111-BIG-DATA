296aa4d3e50fe05c0d9e00542fadd999d89bf33d
elliansjm <ellian98765@gmail.com>
2021-04-08 00:20:06 -0400

    upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from DataExploration import *
 from DataCleaning import *
 from DataAnalysis import *
-from RandomForestRegression import *
-from pyspark.sql.functions import col
+from RegressionModels import *
 
 
 
@@ -10,31 +9,51 @@ from pyspark.sql.functions import col
 
 spark = init_spark()
 
-#code for data analysis and data cleaning
+# Codes for data exploration, cleaning and analysis
 
 df_dataset = spark.read.csv("./data/Train.csv", header="true", inferSchema="true")
-# check_missing_value(df_dataset)
+df_dataset.repartition(1000)
+# Handle missing values
+check_missing_value(df_dataset)
 df_dataset = drop_exception_data(df_dataset)
 
-#visualize_post_by(df_dataset)
-#visualize_under_construction(df_dataset)
-#visualize_rera(df_dataset)
-#visualize_bhk_no(df_dataset)
-#visualize_bhk_or_rk(df_dataset)
-#visualize_square_ft(df_dataset)
-#visualize_ready_to_move(df_dataset)
-#visualize_resale(df_dataset)
+# Data Visualization
+visualize_post_by(df_dataset)
+visualize_under_construction(df_dataset)
+visualize_rera(df_dataset)
+visualize_bhk_no(df_dataset)
+visualize_bhk_or_rk(df_dataset)
+visualize_square_ft(df_dataset)
+visualize_ready_to_move(df_dataset)
+visualize_resale(df_dataset)
 
+# Data cleaning and encoding
 df_dataset = address_to_state(spark, df_dataset)
-# visualize_state(df_dataset)
-df_dataset = target_mean_encode(df_dataset, ['POSTED_BY', 'state'], 'TARGET(PRICE_IN_LACS)')
-# spearman_corr(df_dataset)
-# df_corr_matrix = pearson_corr_heatmap(spark, df_dataset)
-# price_segments(df_dataset) #change price range
-# drop_similar_feature(df_corr_matrix)
-df_dataset = df_dataset.drop('UNDER_CONSTRUCTION', 'READY_TO_MOVE')
-df_dataset.coalesce(1).write.option('header', 'true').csv("./data/train_cleaned")
+visualize_state(df_dataset)
+df_dataset = one_hot_encoding(df_dataset, 'POSTED_BY')
+df_dataset = one_hot_encoding(df_dataset, 'BHK_OR_RK')
+df_dataset = one_hot_encoding(df_dataset, 'state')
+
+# Data analysis
+spearman_corr(df_dataset)
+pearson_corr_heatmap(spark, df_dataset)
+price_segments(df_dataset)
+
+# Drop the features with high similarity with other features
+df_dataset = df_dataset.drop('READY_TO_MOVE','RK','Owner')
+# Perform log transformation on quantitative features of data before feeding the training model
+df_dataset = log_transform(df_dataset, ['BHK_NO','SQUARE_FT','TARGET(PRICE_IN_LACS)'])
 
 
-# code for training model and evaluating model
-#train_model(spark)
+# Codes for training models and evaluating models
+
+# Split the data into training and test sets (20% held out for testing)
+(X_train, X_test) = df_dataset.randomSplit([0.8, 0.2])
+
+# Train random forest model and evaluate the model
+rf_model = train_rf_model(X_train)
+evaluate_model(X_test, rf_model, 'Random Forest Regression')
+
+# Train gbt model and evaluate the model
+gbt_model = train_gbt_model(X_train)
+evaluate_model(X_test, gbt_model, 'GBT Regression')
